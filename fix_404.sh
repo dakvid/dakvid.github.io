b3ee0a7f1df73005bb456af38dbddb8f7cafd6a9
David Friggens <david@frigge.nz>
2016-11-16 14:03:18 +1300

    Fix the 404 fix, to account for external links

--- fix_404.sh
+++ fix_404.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sed -e 's#src="#src="/#g' 404.html > new_404.html
-sed -e 's#href="#href="/#g' new_404.html > 404.html
+sed -e 's_src="\([^h/#]\)_src="/\1_g' 404.html > new_404.html
+sed -e 's_href="\([^h/#]\)_href="/\1_g' new_404.html > 404.html
 rm new_404.html
 
